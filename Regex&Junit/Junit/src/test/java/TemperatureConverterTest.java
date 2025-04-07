e07138288cabce6f109a77901471bee2221d9707
Kartikthakur22 <kartikthakur4861@gmail.com>
2025-04-07 12:06:40 +0530

    [Kartik] Add .Testing Temperature Converter

---
+++ Regex&Junit/Junit/src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import junit.TemperatureConverter;
+import org.junit.jupiter.api.*;
+
+public class TemperatureConverterTest {
+
+    private TemperatureConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new TemperatureConverter();
+    }
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        assertEquals(32.0, converter.celsiusToFahrenheit(0), 0.01);
+        assertEquals(212.0, converter.celsiusToFahrenheit(100), 0.01);
+        assertEquals(98.6, converter.celsiusToFahrenheit(37), 0.01);
+    }
+
+    @Test
+    void testFahrenheitToCelsius() {
+        assertEquals(0.0, converter.fahrenheitToCelsius(32), 0.01);
+        assertEquals(100.0, converter.fahrenheitToCelsius(212), 0.01);
+        assertEquals(37.0, converter.fahrenheitToCelsius(98.6), 0.01);
+    }
+}
+
